36cb6fe8705f9d9d76ddf306a9f3ebc4317c4916
Manzer02 <100516862+Manzer02@users.noreply.github.com>
2022-03-05 08:14:05 +0500

    Update panicusers.lua

--- panicusers.lua
+++ panicusers.lua
@@ -1,2 +1,2 @@
-sequence gigus 768265723w rmkmeirzh ivanbtw69 BonBAROT Masonjen1 Moped Artbom MrRuslan SQWAT
+Donsee sequence gigus 768265723w rmkmeirzh ivanbtw69 BonBAROT Masonjen1 Moped Artbom MrRuslan SQWAT
  
